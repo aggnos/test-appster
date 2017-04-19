cf7fda622fd69f1b485851c27cf79d890c53262d
Aggnos <dotonkovi@gmail.com>
2017-04-20 00:04:26 +0200

    hello

--- source/hello.js
+++ source/hello.js
@@ -1,78 +1,10 @@
-const helloFactory = function ({ React }) {
-  const {
-    string,
-    func
-  } = React.PropTypes;
+<!DOCTYPE html>
+	  <html>
+	  <head>
+	  <title>My first JavaScript page</title>
 
-  return function Hello (props) {
+	  </head>
+	  <body>
 
-    // React wants propTypes & defaultProps
-    // to be static.
-    Hello.propTypes = {
-      word: string,
-      mode: string,
-
-      actions: React.PropTypes.shape({
-        setWord: func.isRequired,
-        setMode: func.isRequired
-      })
-    };
-
-    return {
-
-      props, // set props
-
-
-      componentDidUpdate () {
-        this.refs.wordInput.getDOMNode().focus();
-      },
-
-      render () {
-        const {
-          word,
-          mode
-        } = this.props;
-
-        const {
-          setMode,
-          setWord
-        } = this.props.actions;
-
-        const styles = {
-          displayMode: {
-            display: (mode === 'display') ? 'inline' : 'none'
-          },
-
-          editMode: {
-            display: (mode === 'edit') ? 'inline' : 'none'
-          }
-        };
-
-        const onKeyUp = function (e) {
-          if (e.key !== 'Enter') return;
-
-          setWord(e.target.value);
-          setMode('display');
-        };
-
-        return (
-          <p>Hello,&nbsp;
-            <span
-              style = { styles.displayMode }
-              onClick = { () => setMode('edit') }
-              >{ word }!</span>
-            <input
-              ref = "wordInput"
-              style = { styles.editMode }
-              placeholder = { word }
-              onKeyUp = { onKeyUp } />
-          </p>
-        );
-      }
-    };
-
-  };
-
-};
-
-export default helloFactory;
+	  </body>
+	  </html>
